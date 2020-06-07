9add5851a174d01f38ed55150f289de4330a4e71
Fuwenyue <info@fuwenyue.com>
2020-06-07 15:46:04 +0800

    修改

--- freemycloud.py
+++ freemycloud.py
@@ -2,38 +2,133 @@
 """
 Created on Sat May 23 15:18:14 2020
 invite code:j7hm
-@author: fuwenyue
+@author: fuwenyue 163密码： 4P2V2N9Z2f8F
 """
-import requests,re
+import requests,re,time
 
-url = 'https://freemycloud.xyz/auth/login'
-checkurl = 'https://freemycloud.xyz/user/checkin'
+url = 'https://freemycloud.cc/auth/login'
+checkurl = 'https://freemycloud.cc/user/checkin'
+htmlurl = 'https://freemycloud.cc/user'
+my_proxies = {
+    'http':'socks5://127.0.0.1:10808',
+    'https':'socks5://127.0.0.1:10808'
+    }
+
 datalist = [
         {'email': '492119549@qq.com','passwd': '6N4eTlp6UxWQ','code':'', 'remember_me': 'on'},
-        {'email': 'fuwenyue@outlook.com','passwd': 'Ofo9jK8Vt010','code':'', 'remember_me': 'on'},
         {'email': 'yoval@qq.com','passwd': 'dI86qBN4YjKp','code':'', 'remember_me': 'on'},
-        {'email': 'fuwenyue77@gmail.com','passwd': '4HoNk0JrsbZS','code':'', 'remember_me': 'on'},
         {'email': 'yoval@foxmail.com','passwd': '4P2V2N9Z2f8F','code':'', 'remember_me': 'on'},
+        {'email': '1505044520@qq.com','passwd': '6N4eTlp6UxWQ','code':'', 'remember_me': 'on'},
+        {'email': 'webscraper@qq.com','passwd': 'dI86qBN4YjKp','code':'', 'remember_me': 'on'},
+        {'email': 'webscraper@foxmail.com','passwd': 'dI86qBN4YjKp','code':'', 'remember_me': 'on'}
         ]
-# 登录
-for data in datalist:
-    conn = requests.session()
-    rep = conn.post(url, data=data)
-    repjson = rep.json()
-    print(repjson['msg'])
-    # 签到
-    checkrep = conn.post(checkurl)
-    checkrepjson = checkrep.json()
-    print(checkrepjson['msg'])
-    #剩余流量
-    htmlurl = 'https://freemycloud.xyz/user'
-    htmlrep = conn.get(htmlurl)
-    liuliang = re.findall('<h4 class="m-b-0">(.*)</h4>', htmlrep.text)[2]
-    print('此账号剩余流量：%s'%liuliang)
-'''
-    dingyue = re.findall('data-clipboard-text="(.*)">复制',htmlrep.text)
-    dingyue = list(set(dingyue))
-    print('订阅地址：')
-    for d in dingyue:
-        print(d)
-''' 
+# 订阅链接
+def DingyueUrl(code):
+    # SSR 订阅
+    SSRUrl = 'https://freemycloud.site/link/%s?sub=1'%code
+    # ClashR 订阅
+    ClashRurl = 'https://freemycloud.site/link/%s?clash=2'%code
+    # SS 订阅 
+    SSurl = 'https://freemycloud.site/link/%s?list=shadowrocket'%code
+    # v2ray 订阅
+    v2ray = 'https://freemycloud.site/link/%s?sub=3'%code
+    return [SSRUrl,ClashRurl,SSurl,v2ray]
+
+# 签到
+def Check(datalist):
+    C = input('请输入订阅类型（1：SSR:2：ClashR:3：SS:4：v2ray）：')
+    if C=='1':
+        print('您选择的类型为：SSR')
+        C = 1
+    elif C=='2':
+        print('您选择的类型为：ClashR')
+        C=2
+    elif C=='3':
+        print('您选择的类型为：SS')
+        C=3
+    elif C=='4':
+        print('您选择的类型为：v2ray')
+        C=4
+    else:
+        print('输入错误，默认为：SSR')
+        C=3
+        
+    for data in datalist:
+        conn = requests.session()
+        rep = conn.post(url, data=data,proxies=my_proxies,timeout=5)
+        repjson = rep.json()
+        MSG = repjson['msg']
+        print(MSG)
+        if MSG == '登录成功':
+            pass
+        if MSG =='邮箱不存在':
+            print('账号已被删除，跳过……')
+            continue
+        if MSG == '邮箱或者密码错误':
+            print('请检查账号 %s 密码'%data['email'])
+        # 签到
+        checkrep = conn.post(checkurl,proxies=my_proxies,timeout=5)
+        checkrepjson = checkrep.json()
+        print(checkrepjson['msg'])
+        #剩余流量
+        htmlrep = conn.get(htmlurl,proxies=my_proxies)
+        liuliang = re.findall('<h4 class="m-b-0">(.*)</h4>', htmlrep.text)[2]
+        print('此账号剩余流量：%s'%liuliang)
+        dingyue = re.findall('data-clipboard-text="(.*)">复制',htmlrep.text)
+        dingyue = list(set(dingyue))
+        d = dingyue[0]
+        Code = re.findall(r'link/(.*)\?',d)[0]
+        DingyueUrlList = DingyueUrl(Code)
+        print('订阅连接为：%s'%DingyueUrlList[C-1])
+
+# 注册  
+def REG(datalist):
+    for data in datalist:
+        SENDurl = 'https://freemycloud.xyz/auth/send'
+        print('正在尝试注册账号%s'%data['email'])  
+        SENDdata = {'email': data['email']}
+        print('正在尝试发邮件至:%s'%data['email'])
+        SENDrep = requests.post(SENDurl,data = SENDdata,proxies=my_proxies)
+        print(SENDrep.text)
+        SENDjson = SENDrep.json()
+        SENDmsg = SENDjson['msg']
+        print(SENDmsg)
+        if SENDmsg =='此邮箱已经注册':
+            time.sleep(5)
+            continue
+        REGurl = 'https://freemycloud.xyz/auth/register'
+        emailcode = input('请输入验证码：')
+        REGdata = {
+            'email': data['email'],
+            'name': 'fuwenyue',
+            'passwd': data['passwd'],
+            'repasswd': data['passwd'],
+            'code': 'j7m',
+            'emailcode': emailcode,
+        }
+        REGrep = requests.post(REGurl,data = REGdata,timeout=5,proxies=my_proxies)
+        REGjson = REGrep.json()
+        MSG = REGjson['msg']
+        print(MSG)
+
+#测试代理
+def CheckPorxy():
+    IpUrl = 'https://ip.tour.pub/json'
+    IPReq = requests.get(IpUrl)
+    IPJson = IPReq.json()
+    print('当前IP：%s，'%IPJson['ip'],'所在地区:%s'%IPJson['country'])
+    try:
+        IPReq = requests.get(IpUrl,proxies=my_proxies,timeout=5)
+        IPJson = IPReq.json()
+        print('当前代理IP为：%s，'%IPJson['ip'],'代理所在地区:%s'%IPJson['country'])
+    except:
+        print('代理连接错误')
+
+CheckPorxy()
+S = input('请输入运行模块（1.注册，2.签到）：')
+
+if S =='1':
+    REG(datalist)
+else:
+    Check(datalist)
+    
